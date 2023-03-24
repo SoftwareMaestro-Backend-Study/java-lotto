02d4d329e3fdc1049b6e0b1f557dd81cd7af871c
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-25 02:19:02 +0900

    feat: LottoNumber class 생성 및 테스트

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,14 +3,14 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<LottoNumber> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<LottoNumber> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
