7735f66317fc1e389f49a3daa56d8fc1ee13714d
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-25 02:13:20 +0900

    feat: Money class 생성 및 테스트

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+public class Money {
+    private static final int MIN_VALUE = 1000;
+    private final int value;
+
+    public Money(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value < MIN_VALUE || value % MIN_VALUE != 0) {
+            throw new IllegalArgumentException(String.format("[ERROR] 금액은 %d원일 수 없습니다.", value));
+        }
+    }
+}
