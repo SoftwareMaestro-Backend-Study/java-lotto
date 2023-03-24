59107a83e4d699b6b3282015030eb8020d939e22
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-25 02:40:35 +0900

    feat: Lotto class 내부 필드 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,24 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
     private final List<LottoNumber> numbers;
 
-    public Lotto(List<LottoNumber> numbers) {
+    public Lotto(List<Integer> values) {
+        final List<LottoNumber> numbers = values.stream()
+                .map(LottoNumber::from)
+                .distinct()
+                .collect(Collectors.toList());
         validate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<LottoNumber> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 숫자의 개수는 %d일 수 없습니다.", numbers.size()));
         }
     }
 
