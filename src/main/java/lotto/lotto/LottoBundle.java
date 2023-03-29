d878bb01e4ecd200a7e67f569ae3c8de38d890fc
raycho <raycho521@yonsei.ac.kr>
2023-03-29 22:52:42 +0900

    feat : add LottoBundle

--- src/main/java/lotto/lotto/LottoBundle.java
+++ src/main/java/lotto/lotto/LottoBundle.java
@@ -1,14 +1,17 @@
 package lotto.lotto;
 
+import javafx.scene.input.DataFormat;
+import lotto.purchase.PurchaseService;
 import lotto.util.LottoUtil;
 import lotto.util.UserOutput;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoBundle {
     private List<Lotto> lottos = new ArrayList<>();
-    private Lotto winningLotto ;
+    private Lotto winningLotto;
     private int bonus;
 
 
@@ -22,43 +25,74 @@ public class LottoBundle {
         }
     }
 
-    // 당첨번호 입력받기
-    public void setWinningLotto(List<Integer> winningNumList){
+    /**
+     * 당첨번호 입력받기
+     */
+    public void setWinningLotto(List<Integer> winningNumList) {
         Lotto lotto = new Lotto(winningNumList);
         this.winningLotto = lotto;
     }
 
-    // 보너스번호 입력받기
-    public void setBonus(int bonus){
+    /**
+     * 보너스번호 입력받기
+     */
+    public void setBonus(int bonus) {
         this.bonus = bonus;
     }
 
-    // 로또 출력하기
+    /**
+     * 로또 출력하기
+     */
     public void printLottos() {
         StringBuilder sb = new StringBuilder();
 
-        for (Lotto lotto : lottos){
+        for (Lotto lotto : lottos)
             sb.append(lotto).append("\n");
-        }
 
-        UserOutput.printLottos(0, sb.toString());
+        UserOutput.printLottos(lottos.size(), sb.toString());
     }
 
-    // 당첨 통계 출력하기
+    /**
+     * 당첨 통계 출력하기
+     */
     public void printResult() {
-        String str = "";
-
         int[] prizeStat = getPrizeStat();
         int totalreward = getTotalReward(prizeStat);
+        double rewardPercent = PurchaseService.getRewardPercent(lottos.size(), totalreward);
 
-        UserOutput.printResult(str);
+        StringBuilder sb = new StringBuilder();
+        for (Prize prize : Prize.values()){
+            if (prize == Prize.NONE) continue;
+            sb.append(prize.getToString());
+            sb.append(
+                    String.format(" (%,d원) - %d개",
+                            prize.getReward(),
+                            prizeStat[prize.ordinal()]
+                    ));
+            sb.append("\n");
+        }
+        sb.append(String.format("총 수익률은 %.1f%%입니다.", rewardPercent));
+
+        UserOutput.printResult(sb.toString());
     }
 
     public int[] getPrizeStat() {
-        return new int[Prize.values().length];
+        int[] prizeStat = new int[Prize.values().length];
+        for (Lotto lotto : lottos) {
+            Prize prize = lotto.comparePrize(winningLotto, bonus);
+            if (prize != Prize.NONE)
+                prizeStat[prize.ordinal()]++;
+        }
+        return prizeStat;
     }
-    public int getTotalReward(int[] prizeStat){
-        return 0;
+
+    public int getTotalReward(int[] prizeStat) {
+        int totalReward = 0;
+        for (Prize prize : Prize.values()){
+            if (prizeStat[prize.ordinal()] != 0)
+                totalReward += prize.getReward() * prizeStat[prize.ordinal()];
+        }
+        return totalReward;
     }
-    
+
 }
