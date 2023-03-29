fd8c722596a95bddd27bbe349ed2a7dc3f5c3c20
jeonyuneo <piq2255@gmail.com>
2023-03-29 20:02:15 +0900

    feat: Lottos 추가

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import lotto.domain.generator.LottoGenerator;
+import lotto.domain.result.LottoResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+    private final int quantity;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = new ArrayList<>(lottos);
+        this.quantity = lottos.size();
+    }
+
+    public static Lottos from(LottoGenerator lottoGenerator, PurchasingMoney purchasingMoney) {
+        return new Lottos(Stream.generate(() -> Lotto.from(lottoGenerator))
+                .limit(purchasingMoney.getQuantity())
+                .collect(Collectors.toList()));
+    }
+
+    public List<LottoResult> compare(List<Integer> winningLotto, BonusNumber bonusNumber) {
+        return lottos.stream()
+                .map(lotto -> lotto.getResult(winningLotto, bonusNumber))
+                .collect(Collectors.toList());
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
