fd76161f5115d6814188a2b2a2053eb873cd796c
jeonyuneo <piq2255@gmail.com>
2023-03-29 20:08:09 +0900

    test: ProfitRateTest 추가

---
+++ src/test/java/lotto/domain/result/ProfitRateTest.java
@@ -0,0 +1,19 @@
+package lotto.domain.result;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfitRateTest {
+
+    @Test
+    void 수익률_소숫점_둘째_자리에서_반올림() {
+        // given
+        int prize = 55000;
+        int purchasingMoney = 7000;
+        // when
+        ProfitRate profitRate = ProfitRate.from(prize, purchasingMoney);
+        // then
+        assertThat(profitRate.getValue()).isEqualTo(785.7);
+    }
+}
