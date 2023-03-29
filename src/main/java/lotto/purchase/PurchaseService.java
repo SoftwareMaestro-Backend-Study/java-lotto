0ada73bb6e1639da931b6da7fc19f52d381f597f
raycho <raycho521@yonsei.ac.kr>
2023-03-29 13:12:23 +0900

    feat : add purchase

---
+++ src/main/java/lotto/purchase/PurchaseService.java
@@ -0,0 +1,17 @@
+package lotto.purchase;
+
+public class PurchaseService {
+
+    private final static int PRICE_PER_LOTTO = 1000;
+
+    private PurchaseService() {
+    }
+
+    /**
+     * 텍스트 입력을 lotto 수로 변환
+     */
+    public static int getLottoNum(int cost) {
+        return 0;
+    }
+
+}
