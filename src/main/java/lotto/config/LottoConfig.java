6ba126556be61d9a81c6111d0f03463bfb940de0
raycho <raycho521@yonsei.ac.kr>
2023-03-29 13:12:42 +0900

    feat : add configs

---
+++ src/main/java/lotto/config/LottoConfig.java
@@ -0,0 +1,12 @@
+package lotto.config;
+
+public class LottoConfig {
+
+    private LottoConfig() {
+    }
+
+    public final static int LOTTO_NUM_LENGTH = 6;
+    public final static int LOTTO_MIN_NUM = 1;
+    public final static int LOTTO_MAX_NUM = 45;
+
+}
