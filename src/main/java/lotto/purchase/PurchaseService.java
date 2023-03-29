3c03b63b17bd294cede3f9ae4dbd8698a06d11fa
raycho <raycho521@yonsei.ac.kr>
2023-03-29 22:52:30 +0900

    feat : add PurchaseService

--- src/main/java/lotto/purchase/PurchaseService.java
+++ src/main/java/lotto/purchase/PurchaseService.java
@@ -11,7 +11,17 @@ public class PurchaseService {
      * 텍스트 입력을 lotto 수로 변환
      */
     public static int getLottoNum(int cost) {
-        return 0;
+        if (cost <=0 )
+            throw new IllegalArgumentException("[ERROR] 1 이상의 양수를 입력해주세요");
+        if (cost % PRICE_PER_LOTTO != 0)
+            throw new IllegalArgumentException("[ERROR] 가격의 배수에 해당하는 가격을 입력해주세요");
+
+        return cost / PRICE_PER_LOTTO;
     }
+
+    public static double getRewardPercent(int num, int reward){
+        return (double) reward / (num * PRICE_PER_LOTTO) * 100;
+    }
+
 
 }
