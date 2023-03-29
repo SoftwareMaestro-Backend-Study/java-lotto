ec15121ed4987cbedd300951dddda60ad810d2e3
raycho <raycho521@yonsei.ac.kr>
2023-03-29 13:12:04 +0900

    feat : add utils

---
+++ src/main/java/lotto/util/UserInput.java
@@ -0,0 +1,27 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+    private UserInput() {
+    }
+
+    public static String getCostInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine().trim();
+    }
+
+    public static String getLottoInput(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine().trim();
+    }
+
+    public static String getBonusInput(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine().trim();
+    }
+
+}
