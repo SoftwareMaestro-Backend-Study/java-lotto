44e98a1b4b9a993d5b24890d7bd3555d6115366d
raycho <raycho521@yonsei.ac.kr>
2023-03-30 10:29:54 +0900

    feat : add pried field for Prize

--- src/main/java/lotto/lotto/Prize.java
+++ src/main/java/lotto/lotto/Prize.java
@@ -1,22 +1,51 @@
 package lotto.lotto;
 
 public enum Prize {
-    FIFTH(3, false, 5000, "3개 일치"),
-    FOURTH(4, false, 50000, "4개 일치"),
-    THIRD(5, false, 1500000, "5개 일치"),
-    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치"),
-    FIRST(6, false, 2000000000, "6개 일치");
+    FIFTH(true, 5, 3, false, 5000, "3개 일치"),
+    FOURTH(true, 4, 4, false, 50000, "4개 일치"),
+    THIRD(true, 3, 5, false, 1500000, "5개 일치"),
+    SECOND(true, 2, 5, true, 30000000, "5개 일치, 보너스 볼 일치"),
+    FIRST(true, 1, 6, false, 2000000000, "6개 일치"),
+    NONE(false, 99999, 0, false, 0, "");
 
-
+    private final boolean prized;
+    private final int grade;
     private final int matchNum;
     private final boolean bonus;
     private final int reward;
     private final String toString;
 
-    Prize(int matchNum, boolean bonus, int reward, String toString) {
+    Prize(boolean prized, int grade, int matchNum, boolean bonus, int reward, String toString) {
+        this.prized = prized;
+        this.grade = grade;
         this.matchNum = matchNum;
         this.bonus = bonus;
         this.reward = reward;
         this.toString = toString;
     }
+
+    public boolean isPrized() {
+        return prized;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getMatchNum() {
+        return matchNum;
+    }
+
+    public boolean isBonus() {
+        return bonus;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public String getToString() {
+        return toString;
+    }
+
 }
