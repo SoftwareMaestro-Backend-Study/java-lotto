4cf62264882c4281dffdf3b4d617d17ad2b0f993
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-30 21:59:51 +0900

    refctor: 사용되지 않는 클래스 제거

--- src/main/java/lotto/domain/rankvalidator/RankValidator.java
+++
@@ -1,9 +0,0 @@
-package lotto.domain.rankvalidator;
-
-import lotto.domain.Lotto;
-import lotto.domain.WinNumbers;
-
-public interface RankValidator {
-
-    boolean isRanked(WinNumbers winNumbers, Lotto lotto);
-}
