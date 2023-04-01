05b2a0379eb9601c60b4589606ec50c0c3493ff0
jeonyuneo <piq2255@gmail.com>
2023-04-02 01:09:12 +0900

    style: LottoResult - 상금 값 언더바 추가

--- src/main/java/lotto/domain/result/LottoResult.java
+++ src/main/java/lotto/domain/result/LottoResult.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public enum LottoResult {
 
-    FIRST_PRIZE(6, false, 2000000000, "6개 일치"),
-    SECOND_PRIZE(5, true, 30000000, "5개 일치, 보너스 볼 일치"),
-    THIRD_PRIZE(5, false, 1500000, "5개 일치"),
-    FOURTH_PRIZE(4, false, 50000, "4개 일치"),
-    FIFTH_PRIZE(3, false, 5000, "3개 일치"),
+    FIRST_PRIZE(6, false, 2_000_000_000, "6개 일치"),
+    SECOND_PRIZE(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    THIRD_PRIZE(5, false, 1_500_000, "5개 일치"),
+    FOURTH_PRIZE(4, false, 50_000, "4개 일치"),
+    FIFTH_PRIZE(3, false, 5_000, "3개 일치"),
     NO_PRIZE(0, false, 0, "");
 
     private final int matchingNumber;
