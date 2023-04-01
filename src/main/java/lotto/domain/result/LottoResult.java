083ef2c75e5fdeb909eda5dba79e02782f811cb7
jeonyuneo <piq2255@gmail.com>
2023-04-02 03:21:52 +0900

    refactor: LottoResult - message 필드 삭제 및 toString 추가

--- src/main/java/lotto/domain/result/LottoResult.java
+++ src/main/java/lotto/domain/result/LottoResult.java
@@ -4,23 +4,21 @@ import java.util.Arrays;
 
 public enum LottoResult {
 
-    FIRST_PRIZE(6, false, 2_000_000_000, "6개 일치"),
-    SECOND_PRIZE(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
-    THIRD_PRIZE(5, false, 1_500_000, "5개 일치"),
-    FOURTH_PRIZE(4, false, 50_000, "4개 일치"),
-    FIFTH_PRIZE(3, false, 5_000, "3개 일치"),
-    NO_PRIZE(0, false, 0, "");
+    FIRST_PRIZE(6, false, 2_000_000_000),
+    SECOND_PRIZE(5, true, 30_000_000),
+    THIRD_PRIZE(5, false, 1_500_000),
+    FOURTH_PRIZE(4, false, 50_000),
+    FIFTH_PRIZE(3, false, 5_000),
+    NO_PRIZE(0, false, 0);
 
     private final int matchingNumber;
     private final boolean hasBonusNumber;
     private final int prize;
-    private final String message;
 
-    LottoResult(int matchingNumber, boolean hasBonusNumber, int prize, String message) {
+    LottoResult(int matchingNumber, boolean hasBonusNumber, int prize) {
         this.matchingNumber = matchingNumber;
         this.hasBonusNumber = hasBonusNumber;
         this.prize = prize;
-        this.message = message;
     }
 
     public static LottoResult find(int matchingNumber, boolean hasBonusNumber) {
@@ -35,7 +33,11 @@ public enum LottoResult {
         return prize;
     }
 
-    public String getMessage() {
-        return message;
+    @Override
+    public String toString() {
+        if (hasBonusNumber) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%,d원)", matchingNumber, prize);
+        }
+        return String.format("%d개 일치 (%,d원)", matchingNumber, prize);
     }
 }
