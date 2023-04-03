9852c03952bb3efdeacb3566db68fdf2ddcbc383
raycho <raycho521@yonsei.ac.kr>
2023-04-03 16:02:57 +0900

    feat: edit printLottos

--- src/main/java/lotto/view/UserOutput.java
+++ src/main/java/lotto/view/UserOutput.java
@@ -12,7 +12,10 @@ public class UserOutput {
 
     public static void printLottos(LottoOutputModel model) {
         StringBuilder sb = new StringBuilder();
-        sb.append(model.getLottoCount())
+        sb.append("\n수동으로 ");
+        sb.append(model.getManualCount())
+                .append("개, 자동으로 ");
+        sb.append(model.getAutoCount())
                 .append("개를 구매했습니다.\n");
         for (List<Integer> list : model.getLottoNumbers())
             sb.append(list).append("\n");
@@ -20,7 +23,7 @@ public class UserOutput {
     }
 
     public static void printResult(StatusOutputModel model) {
-        System.out.println("당첨 통계\n" +
+        System.out.println("\n당첨 통계\n" +
                 "---");
         System.out.println(model.getResultStr());
     }
