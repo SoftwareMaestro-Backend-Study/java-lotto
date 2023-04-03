1d4e2b88c2b4be42fe4fce9c8a626d5ab81121ad
great-park <cksgh1735@gmail.com>
2023-04-03 20:56:34 +0900

    feat: 수동 로또 구입 개수를 입력 받는다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,18 +1,26 @@
 package lotto.controller;
 
 import lotto.domain.LottoMoney;
+import lotto.domain.Lottos;
 
 import static lotto.view.Input.insertLottoMoney;
+import static lotto.view.Input.insertManualLottoCount;
 
 public class LottoController {
     public void start() {
         try {
             LottoMoney money = new LottoMoney(insertLottoMoney());
-            // 수동 로또 구매 - 수동 구매 수량 입력 - 수동 구매 번호 입력 -> 생성
+            // 수동 로또 생성 - 수동 구매 수량 입력 - 수동 구매 번호 입력 -> 생성
             // 자동 로또 생성
+            // 모두 담기
+            Lottos lottos = createLottos(money);
             // 지난 주 당첨 번호, 보너스 볼 입력
             // 당첨 통계 출력
         } catch (IllegalArgumentException error) {
         }
+    }
+
+    private Lottos createLottos(LottoMoney money) {
+        int manualLottoCount = insertManualLottoCount();
     }
 }
--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -2,33 +2,31 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static java.lang.Integer.parseInt;
 
 public class Input {
-    private static final String PURCHASEMONEY_REQUEST = "구입금액을 입력해 주세요.";
+    private static final String MONEY_REQUEST = "구입금액을 입력해 주세요.";
+    private static final String MANUAL_LOTTO_COUNT_REQUEST = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String INPUT_IS_NOT_NUMBER = "문자열이 아닌 숫자(정수)를 입력해주세요.";
 
     public static int insertLottoMoney() {
-        System.out.println(PURCHASEMONEY_REQUEST);
+        System.out.println(MONEY_REQUEST);
+        String lottoMoney = Console.readLine();
 
-        String purchaseMoney = Console.readLine();
-        checkNumber(purchaseMoney);
-        return parseInt(purchaseMoney);
+        return checkNumber(lottoMoney);
     }
 
-    private static void checkNumber(String input) {
+    public static int insertManualLottoCount() {
+        System.out.println(MANUAL_LOTTO_COUNT_REQUEST);
+        String manualLottoCount = Console.readLine();
+
+        return checkNumber(manualLottoCount);
+    }
+
+    private static int checkNumber(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException(INPUT_IS_NOT_NUMBER);
         }
-    }
-
-    private static List<Integer> parsePrizeNumbers(List<String> splitedInput) {
-        return splitedInput.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        return parseInt(input);
     }
 }
